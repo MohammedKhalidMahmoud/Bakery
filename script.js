d73a2a60992beb8c983bbbf32faa8b18f3d1d6e6
Mohamed Khaled <76552489+MohammedKhalidMahmoud@users.noreply.github.com>
2025-02-02 10:45:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let nav=document.querySelector(".nav");
+let icon=document.querySelector("nav .logo");
+let tabs=document.querySelectorAll("nav li");
+
+// window.addEventListener('scroll', function(){
+//     if(window.scrollY > 35){
+//         nav.classList.add('header-scrolled');
+//         icon.src="./images/bakery-color.png";
+//         tabs.forEach((hr)=>{
+//             hr.style.color="gold";
+//         })
+//     }
+//     if(window.scrollY<35){
+//         nav.classList.add('header-in-position');
+//         icon.src="./images/bakery-light-1.png";
+//         tabs.forEach((hr)=>{
+//             hr.style.color="white";
+//         })
+//     }
+
+        
+// });
