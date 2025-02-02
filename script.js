fcb094d80af0938d75d129c1519c44b1527bfeaa
Mohamed Khaled <76552489+MohammedKhalidMahmoud@users.noreply.github.com>
2025-02-02 14:33:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-let nav=document.querySelector(".nav");
-let icon=document.querySelector("nav .logo");
-let tabs=document.querySelectorAll("nav li");
+const list=document.querySelector(".ll");
+const icon=document.querySelector("ul .logo");
+const tabs=document.querySelectorAll(".sp");
 
-// window.addEventListener('scroll', function(){
-//     if(window.scrollY > 35){
-//         nav.classList.add('header-scrolled');
-//         icon.src="./images/bakery-color.png";
-//         tabs.forEach((hr)=>{
-//             hr.style.color="gold";
-//         })
-//     }
-//     if(window.scrollY<35){
-//         nav.classList.add('header-in-position');
-//         icon.src="./images/bakery-light-1.png";
-//         tabs.forEach((hr)=>{
-//             hr.style.color="white";
-//         })
-//     }
+window.addEventListener('scroll', function(){
+    if(window.scrollY < 30){
+        list.style.backgroundColor="transparent";
+        icon.src="./images/bakery-light-1.png";
+        tabs.forEach((hr)=>{
+            hr.style.color="white";
+        })
+        
+        
+    }
+    else if(this.window.scrollY>=30){
+        console.log("scrolling");
+        list.style.backgroundColor="white";
+        icon.src="./images/bakery-color.png";
+        tabs.forEach((hr)=>{
+            hr.style.color="black";
+        });
+    }
 
         
-// });
+});
