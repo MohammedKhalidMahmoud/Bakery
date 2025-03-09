0109a018d8dabb434369de51f3651fa668d96279
mkhaled <mohammedkhalidmahmoud@gmail.com>
2025-03-09 21:45:54 +0200

    done with typescript

--- script.ts
+++ script.ts
@@ -1,7 +1,8 @@
 const nav : HTMLElement | null =document.querySelector(".nav");
 const icon : HTMLImageElement | null=document.querySelector(".nav .logo");
-const tabs : NodeListOf<HTMLElement>=document.querySelectorAll(".sp");
+const tabs : NodeListOf<HTMLElement>=document.querySelectorAll(".span");
 
+console.log(tabs.length);
 window.addEventListener('scroll', ()=>{
     if(window.scrollY < 30){
         nav? nav.style.backgroundColor="transparent" : console.log("nav is null");
@@ -14,8 +15,8 @@ window.addEventListener('scroll', ()=>{
     }
     else if(window.scrollY>=30){
         console.log("scrolling");
-        nav.style.backgroundColor="white";
-        icon.src="./images/bakery-color.png";
+        nav? nav.style.backgroundColor="white" : console.log("nav is null");
+        icon? icon.src="./images/bakery-color.png" : console.log("icon is null");
         tabs.forEach((hr)=>{
             hr.style.color="black";
         });
--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,20 @@
 "use strict";
 const nav = document.querySelector(".nav");
 const icon = document.querySelector(".nav .logo");
-const tabs = document.querySelectorAll(".sp");
+const tabs = document.querySelectorAll(".span");
+console.log(tabs.length);
 window.addEventListener('scroll', () => {
     if (window.scrollY < 30) {
-        nav.style.backgroundColor = "transparent";
-        icon.src = "./images/bakery-light-1.png";
+        nav ? nav.style.backgroundColor = "transparent" : console.log("nav is null");
+        icon ? icon.src = "./images/bakery-light-1.png" : console.log("icon is null");
         tabs.forEach((hr) => {
             hr.style.color = "white";
         });
     }
     else if (window.scrollY >= 30) {
         console.log("scrolling");
-        nav.style.backgroundColor = "white";
-        icon.src = "./images/bakery-color.png";
+        nav ? nav.style.backgroundColor = "white" : console.log("nav is null");
+        icon ? icon.src = "./images/bakery-color.png" : console.log("icon is null");
         tabs.forEach((hr) => {
             hr.style.color = "black";
         });
