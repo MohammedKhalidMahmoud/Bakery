6b3cea7571da720f78f22513dbae9d18a9548130
mkhaled <mohammedkhalidmahmoud@gmail.com>
2025-03-09 21:39:30 +0200

    added typescript successfully

---
+++ script.ts
@@ -0,0 +1,25 @@
+const nav : HTMLElement | null =document.querySelector(".nav");
+const icon : HTMLImageElement | null=document.querySelector(".nav .logo");
+const tabs : NodeListOf<HTMLElement>=document.querySelectorAll(".sp");
+
+window.addEventListener('scroll', ()=>{
+    if(window.scrollY < 30){
+        nav? nav.style.backgroundColor="transparent" : console.log("nav is null");
+        icon? icon.src="./images/bakery-light-1.png" : console.log("icon is null");
+        tabs.forEach((hr)=>{
+            hr.style.color="white";
+        })
+        
+        
+    }
+    else if(window.scrollY>=30){
+        console.log("scrolling");
+        nav.style.backgroundColor="white";
+        icon.src="./images/bakery-color.png";
+        tabs.forEach((hr)=>{
+            hr.style.color="black";
+        });
+    }
+
+        
+});
--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,21 @@
-const nav=document.querySelector(".nav");
-const icon=document.querySelector(".nav .logo");
-const tabs=document.querySelectorAll(".sp");
-
-window.addEventListener('scroll', function(){
-    if(window.scrollY < 30){
-        nav.style.backgroundColor="transparent";
-        icon.src="./images/bakery-light-1.png";
-        tabs.forEach((hr)=>{
-            hr.style.color="white";
-        })
-        
-        
-    }
-    else if(window.scrollY>=30){
-        console.log("scrolling");
-        nav.style.backgroundColor="white";
-        icon.src="./images/bakery-color.png";
-        tabs.forEach((hr)=>{
-            hr.style.color="black";
+"use strict";
+const nav = document.querySelector(".nav");
+const icon = document.querySelector(".nav .logo");
+const tabs = document.querySelectorAll(".sp");
+window.addEventListener('scroll', () => {
+    if (window.scrollY < 30) {
+        nav.style.backgroundColor = "transparent";
+        icon.src = "./images/bakery-light-1.png";
+        tabs.forEach((hr) => {
+            hr.style.color = "white";
         });
     }
-
-        
+    else if (window.scrollY >= 30) {
+        console.log("scrolling");
+        nav.style.backgroundColor = "white";
+        icon.src = "./images/bakery-color.png";
+        tabs.forEach((hr) => {
+            hr.style.color = "black";
+        });
+    }
 });
