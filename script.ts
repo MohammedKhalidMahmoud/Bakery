4abd733bc9c4e98f438a2a3baf595336ef84b26f
mkhaled <mohammedkhalidmahmoud@gmail.com>
2025-03-11 10:36:18 +0200

    add function scrolling to typescript code

--- script.ts
+++ script.ts
@@ -3,24 +3,28 @@ const icon : HTMLImageElement | null=document.querySelector(".nav .logo");
 const tabs : NodeListOf<HTMLElement>=document.querySelectorAll(".span");
 
 console.log(tabs.length);
-window.addEventListener('scroll', ()=>{
-    if(window.scrollY < 30){
-        nav? nav.style.backgroundColor="transparent" : console.log("nav is null");
-        icon? icon.src="./images/bakery-light-1.png" : console.log("icon is null");
-        tabs.forEach((hr)=>{
-            hr.style.color="white";
-        })
-        
-        
-    }
-    else if(window.scrollY>=30){
-        console.log("scrolling");
-        nav? nav.style.backgroundColor="white" : console.log("nav is null");
-        icon? icon.src="./images/bakery-color.png" : console.log("icon is null");
-        tabs.forEach((hr)=>{
-            hr.style.color="black";
-        });
-    }
 
-        
-});
+const scrolling  = (): void => {
+    window.addEventListener('scroll', ()=>{
+        if(window.scrollY < 30){
+            nav? nav.style.backgroundColor="transparent" : console.log("nav is null");
+            icon? icon.src="./images/bakery-light-1.png" : console.log("icon is null");
+            tabs.forEach((hr)=>{
+                hr.style.color="white";
+            })
+            
+            
+        }
+        else if(window.scrollY>=30){
+            console.log("scrolling");
+            nav? nav.style.backgroundColor="white" : console.log("nav is null");
+            icon? icon.src="./images/bakery-color.png" : console.log("icon is null");
+            tabs.forEach((hr)=>{
+                hr.style.color="black";
+            });
+        }
+    });
+}
+
+
+scrolling();
--- scripts/script.js
+++ scripts/script.js
@@ -3,20 +3,23 @@ const nav = document.querySelector(".nav");
 const icon = document.querySelector(".nav .logo");
 const tabs = document.querySelectorAll(".span");
 console.log(tabs.length);
-window.addEventListener('scroll', () => {
-    if (window.scrollY < 30) {
-        nav ? nav.style.backgroundColor = "transparent" : console.log("nav is null");
-        icon ? icon.src = "./images/bakery-light-1.png" : console.log("icon is null");
-        tabs.forEach((hr) => {
-            hr.style.color = "white";
-        });
-    }
-    else if (window.scrollY >= 30) {
-        console.log("scrolling");
-        nav ? nav.style.backgroundColor = "white" : console.log("nav is null");
-        icon ? icon.src = "./images/bakery-color.png" : console.log("icon is null");
-        tabs.forEach((hr) => {
-            hr.style.color = "black";
-        });
-    }
-});
+const scrolling = () => {
+    window.addEventListener('scroll', () => {
+        if (window.scrollY < 30) {
+            nav ? nav.style.backgroundColor = "transparent" : console.log("nav is null");
+            icon ? icon.src = "./images/bakery-light-1.png" : console.log("icon is null");
+            tabs.forEach((hr) => {
+                hr.style.color = "white";
+            });
+        }
+        else if (window.scrollY >= 30) {
+            console.log("scrolling");
+            nav ? nav.style.backgroundColor = "white" : console.log("nav is null");
+            icon ? icon.src = "./images/bakery-color.png" : console.log("icon is null");
+            tabs.forEach((hr) => {
+                hr.style.color = "black";
+            });
+        }
+    });
+};
+scrolling();
